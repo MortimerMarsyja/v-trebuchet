e6ae42f04d9da472f479d079c6782b5f96692ca8
Marcos Collado <marcoscolladosegura@ied.edu>
2022-12-20 15:30:32 +0100

    add cypress

--- src/02-pages/Landing/Landing.tsx
+++ src/02-pages/Landing/Landing.tsx
@@ -1,18 +1,20 @@
-import trebuchet from '@assets/treb.svg';
+import Logo from "@components/Logo/Logo";
 
 const Landing = () => {
-
-    return (
-        <div>
-            <div className='flex flex-col items-center'>
-                <img src={trebuchet} className="my-8" width={500} alt="trebuchet logo" />
-                <div className="text-3xl font-bold bg-color-regal-blue w-full h-full text-white flex justify-center">
-                    Trebuchet
-                </div>
-            </div>
-            <p className='text-2xl w-full flex justify-center text-light m-3'>V<sub>0.0.1</sub></p>
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        {/* <img src={trebuchet} className="my-8" width={500} alt="trebuchet logo" /> */}
+        <Logo backgroundColor="#37337" size={500} />
+        <div className="text-3xl font-bold bg-color-regal-blue w-full h-full text-white flex justify-center">
+          Trebuchet
         </div>
-    )
-}
+      </div>
+      <p className="text-2xl w-full flex justify-center text-light m-3">
+        V<sub>0.0.1</sub>
+      </p>
+    </div>
+  );
+};
 
 export default Landing;
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { mergeConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const ASSET_URL = process.env.ASSET_URL || "";
+
 export default {
   stories: [
     "../stories/**/*.stories.mdx",
@@ -9,6 +11,11 @@ export default {
   ],
   addons: ["@storybook/addon-essentials", "storybook-addon-designs"],
   framework: "@storybook/react-vite",
+  server: {
+    type: "vite-dev-server",
+    host: "localhost",
+    port: 3000,
+  },
   plugins: [svgr(), tsconfigPaths()],
   async viteFinal(config) {
     // Merge custom configuration into the default config
